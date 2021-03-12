559e4589b197ef0c47d55864599e24cdd5422f82
kristofre <kristof.renders@dynatrace.com>
2021-03-12 13:15:21 +0100

    Add Error event

--- src/com/dynatrace/ace/Event.groovy
+++ src/com/dynatrace/ace/Event.groovy
@@ -253,4 +253,67 @@ def pushDynatraceInfoEvent( Map args ) {
   return true
 }
 
+def pushDynatraceErrorEvent( Map args ) {
+
+  // check input arguments
+  String dtTenantUrl = args.containsKey("dtTenantUrl") ? args.dtTenantUrl : "${DT_TENANT_URL}"
+  String dtApiToken = args.containsKey("dtApiToken") ? args.dtApiToken : "${DT_API_TOKEN}"
+  def tagRule = args.containsKey("tagRule") ? args.tagRule : ""
+  String source = args.containsKey("source") ? args.source : "Jenkins"
+
+  String description = args.containsKey("description") ? args.description : ""
+  String title = args.containsKey("title") ? args.title : ""
+
+  def customProperties = args.containsKey("customProperties") ? args.customProperties : [ ]
+
+  // check minimum required params
+  if(tagRule == "" ) {
+    echo "tagRule is a mandatory parameter!"
+    return 1
+  }
+  if(source == "" ) {
+      echo "source is a mandatory parameter!"
+      return 1
+  }
+  if(description == "" ) {
+      echo "description is a mandatory parameter!"
+      return 1
+  }
+  
+  if(title == "" ) {
+      echo "title is a mandatory parameter!"
+      return 1
+  }
+
+  String eventType = "ERROR_EVENT"
+
+  def postBody = [
+    eventType: eventType,
+    attachRules: [tagRule: tagRule],
+    description: description,
+    title: title,
+    customProperties: customProperties,
+    source: source,
+    tags: tagRule[0].tags
+  ]
+
+  def createEventResponse = httpRequest contentType: 'APPLICATION_JSON', 
+    customHeaders: [[maskValue: true, name: 'Authorization', value: "Api-Token ${dtApiToken}"]], 
+    httpMode: 'POST',
+    requestBody: JsonOutput.toJson(postBody),
+    responseHandle: 'STRING',
+    url: "${dtTenantUrl}/api/v1/events",
+    validResponseCodes: "200:403",
+    ignoreSslErrors: true
+
+    if (createEventResponse.status == 200) {
+      echo "Custom info event posted successfully!"
+    } else {
+      echo "Failed To post event:" + createEventResponse.content
+      return false
+    }
+
+  return true
+}
+
 return this
